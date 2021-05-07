235bf9dadb504bb04184c5a9cc6908316847e07d
cnaru7 <48192815+cnaru7@users.noreply.github.com>
2021-05-07 13:45:43 -0400

    Update logicJS.js

--- logicJS.js
+++ logicJS.js
@@ -89,6 +89,7 @@ function solve(formula){
     var temp = unique(formula);
     alert(totalCount(formula));
     for(i = 0; i <= totalCount(formula); i++){
+        alert(i);
         alert(i.toString(2));
     }
     /** 
